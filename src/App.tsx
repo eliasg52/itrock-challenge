1e81ea5cdf6046680f0cef7806ad1c2f07f14c47
Elias Joaquin Garcia <eliasgarcia81@gmail.com>
2025-08-22 12:26:19 -0300

    fixed status bar in android device

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import Navigation from '../src/navigation/Navigation';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { UserProvider } from './context/useUserContext';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import Navigation from "../src/navigation/Navigation";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { UserProvider } from "./context/useUserContext";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="dark" />
       <UserProvider>
         <QueryClientProvider client={queryClient}>
           <Navigation />
